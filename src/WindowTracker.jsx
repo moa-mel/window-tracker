5964000ec520745fdc8dba1edcbda8619f5ede98
Olaitan <olaitanmaryakano@gmail.com>
2023-11-08 13:29:13 +0100

    initial commit

---
+++ src/WindowTracker.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const WindowTracker = () => {
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+    
+    React.useEffect(() => {
+        function watchWidth() {
+            console.log("Setting up...")
+            setWindowWidth(window.innerWidth)
+        }
+        
+        window.addEventListener("resize", watchWidth)
+        
+        return function() {
+            console.log("Cleaning up...")
+            window.removeEventListener("resize", watchWidth)
+        }
+    }, [])
+
+  return (
+    <h1>Window width: {windowWidth}</h1>
+  )
+}
+
+export default WindowTracker
